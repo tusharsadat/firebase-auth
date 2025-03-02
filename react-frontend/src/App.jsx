958e8687c7b6528c48428c30c1a56864ad35f768
tushar-sadat <tusharsadat30@gmail.com>
2025-03-03 03:34:08 +0600

    Improve Routing in App.js

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import Login from "./components/Login";
-import PrivateRoute from "./route/PrivateRoute";
-import Dashboard from "./components/Dashboard";
+import AppNavbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
+        <AppNavbar />
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route
             path="/dashboard"
             element={
